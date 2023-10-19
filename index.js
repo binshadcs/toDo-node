a4ce8c9243a7d1f9685f8acb43b65fe42b0a4c44
binshadcs <binshadcs@gmail.com>
2023-10-19 17:39:17 +0530

    gitignore file updated specific for node

--- index.js
+++ index.js
@@ -3,10 +3,23 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+let list = [];
+
 
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/add", (req, res) => {
+    
+    list.push(req.body['text']);
+    let data = { list : list }
+    res.render("index.ejs", data);
+})
+
+app.get('/', (req, res)=> {
+    res.render("index.ejs");
+})
 
 app.listen(port, ()=> {
     console.log(`Running server at port ${port}`);
